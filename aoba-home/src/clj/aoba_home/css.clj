00d829dba8b867e5e0e4c7d8d704ded382b8fe03
Oyama WSL <oyama@nexcert.vn>
2020-05-26 16:10:33 +0700

    リファクタリングとスタイル修正

--- aoba-home/src/clj/aoba_home/css.clj
+++ aoba-home/src/clj/aoba_home/css.clj
@@ -5,4 +5,11 @@
   ;; [:body {:color "red"}]
   ;; [:.level1 {:color "green"}]
   [:p
-   [:span {:margin-left "0.7em"}]])
+   [:span {:margin-left "0.7em"}]]
+  [:i {:padding-top "0.4em"
+       :padding-left "0.2em"}]
+  [:.home-panel {:margin "1em"}]
+  [:.headline {:margin-left "0.7em"}]
+  [:.work-panel {:margin-left "3em"}]
+  [:.description {:margin-left "0.5em"}]
+  )
--- aoba-home/src/cljs/aoba_home/views.cljs
+++ aoba-home/src/cljs/aoba_home/views.cljs
@@ -8,15 +8,23 @@
 
 ;; home
 
+(def clojure-official {:url "https://clojure.org/" :name "Clojure"})
+(def clojurescript-official {:url "https://clojurescript.org/" :name "ClojureScript"})
+(def re-frame-official {:url "https://day8.github.io/re-frame/" :name "re-frame"})
+(def genegacha-techs [clojurescript-official
+                      re-frame-official
+                      {:url "http://quil.info/" :name "Quil"}])
+(def dmm-gacha-techs [clojure-official
+                      clojurescript-official
+                      re-frame-official
+                      {:url "https://github.com/ring-clojure/ring" :name "Ring"}
+                      {:url "https://affiliate.dmm.com/api/" :name "DMM Webサービス"}])
+(def br [:br])
+
 (defn home-title []
   [re-com/title
    :label "aoba17"
    :level :level1])
-
-(defn link-to-about-page []
-  [re-com/hyperlink-href
-   :label "go to About Page"
-   :href "#/about"])
 
 (defn url-link
   ""
@@ -27,50 +35,49 @@
     :href url
     :target "_blank"]])
 
-(def br [:br])
-
-(def clojure-official {:url "https://clojure.org/" :name "Clojure"})
-(def clojurescript-official {:url "https://clojurescript.org/" :name "ClojureScript"})
-(def re-frame-official {:url "https://day8.github.io/re-frame/" :name "re-frame"})
-
-(def dmm-gacha-techs [clojure-official
-                      clojurescript-official
-                      re-frame-official
-                      {:url "https://github.com/ring-clojure/ring" :name "Ring"}
-                      {:url "https://affiliate.dmm.com/api/" :name "DMM Webサービス"}])
-
 (defn work-panel
   ""
   [name url techs]
   [re-com/v-box
-   :gap "1em"
+   :gap "0.4em"
    :class "work-panel"
    :children [[url-link
                url
                [re-com/title
                 :label name
                 :level :level3]]
-              [re-com/p
+              [re-com/p {:class "description"}
                "This space is for the descriptions of the app."
                br
                "Techs :"
                (for [{:keys [url name]} techs]
                  [url-link url name])]]])
 
+(defn headline
+  ""
+  [label]
+  [re-com/h-box
+   :align :center
+   :children [[:i.fas.fa-caret-right.fa-2x]
+              [re-com/title
+               :label label
+               :level :level2
+               :class "headline"]]])
 
 (defn home-panel []
   [re-com/v-box
    :gap "1em"
    :class "home-panel"
    :children [[home-title]
-              [re-com/title
-               :label "公開中のサービス"
-               :level :level2]
+              [headline "公開中のサービス"]
+              [work-panel
+               "ジェネラティブガチャガチャシミュレーター"
+               "https://www.genegacha.com"
+               genegacha-techs]
               [work-panel
                "出会って4秒でサンプル動画"
                "https://av.genegacha.com"
-               dmm-gacha-techs]
-              [link-to-about-page]]])
+               dmm-gacha-techs]]])
 
 
 ;; about
