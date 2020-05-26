bcccc402c4dcacba0b75b59a5adbde6eda9dfbab
Oyama WSL <oyama@nexcert.vn>
2020-05-26 14:56:15 +0700

    公開中のサービス として 『出会って4秒でサンプル動画』を追加

--- aoba-home/src/clj/aoba_home/css.clj
+++ aoba-home/src/clj/aoba_home/css.clj
@@ -2,5 +2,7 @@
   (:require [garden.def :refer [defstyles]]))
 
 (defstyles screen
-  [:body {:color "red"}]
-  [:.level1 {:color "green"}])
+  ;; [:body {:color "red"}]
+  ;; [:.level1 {:color "green"}]
+  [:p
+   [:span {:margin-left "0.7em"}]])
--- aoba-home/src/cljs/aoba_home/views.cljs
+++ aoba-home/src/cljs/aoba_home/views.cljs
@@ -9,22 +9,68 @@
 ;; home
 
 (defn home-title []
-  (let [name (re-frame/subscribe [::subs/name])]
-    [re-com/title
-     :label (str "Hello from " @name ". This is the Home Page.")
-     :level :level1]))
+  [re-com/title
+   :label "aoba17"
+   :level :level1])
 
 (defn link-to-about-page []
   [re-com/hyperlink-href
    :label "go to About Page"
    :href "#/about"])
 
+(defn url-link
+  ""
+  [url label]
+  [:span
+   [re-com/hyperlink-href
+    :label label
+    :href url
+    :target "_blank"]])
+
+(def br [:br])
+
+(def clojure-official {:url "https://clojure.org/" :name "Clojure"})
+(def clojurescript-official {:url "https://clojurescript.org/" :name "ClojureScript"})
+(def re-frame-official {:url "https://day8.github.io/re-frame/" :name "re-frame"})
+
+(def dmm-gacha-techs [clojure-official
+                      clojurescript-official
+                      re-frame-official
+                      {:url "https://github.com/ring-clojure/ring" :name "Ring"}
+                      {:url "https://affiliate.dmm.com/api/" :name "DMM Webサービス"}])
+
+(defn work-panel
+  ""
+  [name url techs]
+  [re-com/v-box
+   :gap "1em"
+   :class "work-panel"
+   :children [[url-link
+               url
+               [re-com/title
+                :label name
+                :level :level3]]
+              [re-com/p
+               "This space is for the descriptions of the app."
+               br
+               "Techs :"
+               (for [{:keys [url name]} techs]
+                 [url-link url name])]]])
+
+
 (defn home-panel []
   [re-com/v-box
    :gap "1em"
+   :class "home-panel"
    :children [[home-title]
-              [link-to-about-page]
-              ]])
+              [re-com/title
+               :label "公開中のサービス"
+               :level :level2]
+              [work-panel
+               "出会って4秒でサンプル動画"
+               "https://av.genegacha.com"
+               dmm-gacha-techs]
+              [link-to-about-page]]])
 
 
 ;; about
